bb65bc621996341b13a28ad233a9081ffec4b627
Per Rovegård <per@rovegard.se>
2021-06-29 20:15:15 +0200

    Add logging

--- Program.cs
+++ Program.cs
@@ -9,9 +9,14 @@ namespace ironpdf_crash
         {
             License.LicenseKey = "";
 
-            Console.WriteLine("OS:   " + Environment.OSVersion);
-            Console.WriteLine(".NET: " + Environment.Version);
-            Console.WriteLine("Iron: " + VersionInfo.IronPdfAssemblyVersion);
+            // IronPdf.Logging.Logger.EnableDebugging = true;
+            IronPdf.Logging.Logger.LogFilePath = "Default.log";
+            IronPdf.Logging.Logger.LoggingMode = IronPdf.Logging.Logger.LoggingModes.All;
+            
+            Console.WriteLine("OS:     " + Environment.OSVersion);
+            Console.WriteLine(".NET:   " + Environment.Version);
+            Console.WriteLine("Iron:   " + VersionInfo.IronPdfAssemblyVersion);
+            Console.WriteLine("Deploy: " + Installation.ActualDeploymentPath);
 
             var pdf = new PdfDocument("sample.pdf");
             _ = pdf.ToBitmap();
